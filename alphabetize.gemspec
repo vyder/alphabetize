8853a23a6bd1cb4944ec30f53d9550972a8406e8
Vidur Murali <vidur.murali@gmail.com>
2012-06-25 18:19:23 -0400

    Added bin/alphabetize; Details

--- alphabetize.gemspec
+++ alphabetize.gemspec
@@ -4,9 +4,9 @@ require File.expand_path('../lib/alphabetize/version', __FILE__)
 Gem::Specification.new do |gem|
   gem.authors       = ["Vidur Murali"]
   gem.email         = ["vidur.murali@gmail.com"]
-  gem.description   = %q{TODO: Write a gem description}
-  gem.summary       = %q{TODO: Write a gem summary}
-  gem.homepage      = ""
+  gem.description   = %q{Alphabetizes your gemfile}
+  gem.summary       = %q{Please check how far I've got with the file parsing before you use this gem. It does create a backup of your gemfile in case things go seriously wrong.'}
+  gem.homepage      = "https://github.com/vyder/alphabetize"
 
   gem.files         = `git ls-files`.split($\)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,37 @@
 require "alphabetize/version"
 
 module Alphabetize
-  # Your code goes here...
+  def alphabetize_file
+    file = File.new(filename)
+
+    lines = file.readlines
+    lineHash = gem_hash(lines)
+
+    backupFilename = "old_#{filename}"
+    %x( mv #{filename} #{backupFilename})
+
+    file = File.open(filename, 'w')
+    file.truncate(0) # clear the file
+    lineHash.keys.sort.each do |gem|
+      line = lineHash[gem]
+      file.puts(line)
+    end
+    file.close
+
+  end
+
+  def gem_hash(lines)
+    hash = {}
+
+    lines.each do |line|
+      match_data = line.scan(/(\"([^"]*)\")|(\'([^']*)\')/)
+      # The gem is either the second element or the 4th element of the array
+      gem ||= match_data[0][1]
+      gem ||= match_data[0].last
+
+      hash[gem] = line
+    end
+
+    hash
+  end
 end
